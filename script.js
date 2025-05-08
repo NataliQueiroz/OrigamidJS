fd87a65ee57ca824d2dc2a105dfb71c9bb7e91ed
Natinha <queiroznatt@gmail.com>
2025-05-07 20:27:34 -0400

    Aula de variáveis + exercício de fixação

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Exercício 1
+Declarar uma variável com seu nome 
+Declarar uma variável com sua idade 
+Declarar uma variável com sua comida preferida e não atribuir valor
+Atribuir valor a sua comida preferida
+Declarar 5 variáveis diferentes sem valores.
+*/
+
+var nome = 'Natali';
+console.log(nome)
+
+var idade = 30;
+console.log(idade)
+
+var comidaPreferida 
+comidaPreferida = 'Sorvete'
+console.log(comidaPreferida)
+
+var gatos 
+var vasilha
+var lesma
+var coração 
+var sibito // pode também ser separado por vírgula, sem a nessecidade de ficar colocando var em toda linha.
+
+console.log(gatos, vasilha, lesma,coração,sibito)
+
