5e77906527ab406aa2b74e6ae9a479248dc69f5b
Natali Queiróz <queiroznatt@gmail.com>
2025-06-16 21:44:59 -0400

    Aula1 de Números e Operadores

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-// Declare uma variável contendo uma string
-var umaString = "Olar casada";
-console.log(umaString);
-// Declare uma variável contendo um número dentro de uma string
- var numeroemString = "28456";
-console.log(numeroemString);
-// Declare uma variável com a sua idade
-  var umaIdade = 30;
-console.log(umaIdade);
-// Declare duas variáveis, uma com seu nome
-// e outra com seu sobrenome e some as mesmas
-var meuNome = "Natali";
-var meuSobrenome = "Queiroz";
-var nomeCompleto = meuNome + " " + meuSobrenome;
-console.log(nomeCompleto);
-// Coloque a seguinte frase em uma variável: It's time
-var frase = "It's time";
-console.log(frase);
+var total1 = 10 + 5;
+var divisao = 200 / 5;
 
-// Verifique o tipo da variável que contém o seu nome
-typeof (meuNome);
-console.log(typeof meuNome)
+console.log(divisao)
+
+var testeNaN = 'Isso é 100' / 2;
+console.log(testeNaN)
