431fa6a75abab6eaa0bee2bdc4042cfe41315f1e
Natali Queiróz <queiroznatt@gmail.com>
2025-06-24 22:07:24 -0400

    Funções 1 e 2

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
-// Verifique se a sua idade é maior do que a de algum parente
-// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
-var idadeNat= 30;
-var idadeLouise = 31;
-if (idadeNat > idadeLouise){
-  console.log('É maior')
-} else if (idadeNat === idadeLouise){
-  console.log('É igual')
-} else{
-  console.log('É menor')
+function areaQuadrado(lado){
+  return lado * lado;
+}
+
+console.log(areaQuadrado(2));
+
+function pi() {
+  return 3.14;
+}
+
+var total = 5 * pi(); // 15.7
+console.log(pi());
+
+function imc(peso,altura) {
+    var imc = peso / altura * altura;
+    return imc;
+}
+console.log(imc(61, 1,56));
+
+function corFavorita(cor) {
+  if(cor === 'azul') {
+    return 'Você gosta do céu';
+  } else if(cor === 'verde') {
+    return 'Você gosta de mato';
+  } else {
+    return 'Você não gosta de nada';
+  }
+};
+corFavorita(); 
+
+addEventListener('click', function(){console.log('Oi')});
+
+function imc2(peso, altura) {
+  const imc = peso / (altura ** 2);
+  console.log(imc);
+}
+
+imc2(80, 1.80); // retorna o imc
+console.log(imc2(80, 1.80)); // retorna o imc e undefined
+
+function terceiraIdade(idade) {
+  if(typeof idade !== 'number') {
+    return 'Por gentileza preencha um número'
+  }
+ else if (idade >= 60){
+    return true;
+  } else {
+    return false;
+  }
 };
 
-// Qual valor é retornado na seguinte expressão?
-var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(terceiraIdade('vaca'));
+  
+var totalPaises = 193;
+function faltaVisitar(paisesVisitados){
 
-// Verifique se as seguintes variáveis são Truthy ou Falsy
-var nome = 'Andre';
-var idade = 28;
-var possuiDoutorado = false;
-var empregoFuturo;
-var dinheiroNaConta = 0;
-
-console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
-
-// Compare o total de habitantes do Brasil com China (valor em milhões)
-var brasil = 207;
-var china = 1340;
-
-if (brasil > china) {
-  console.log('Brasil tem mais habitantes')
-} else {
-  console.log('Brasil tem menos habitantes')
-};
-
-// O que irá aparecer no console?
-if(('Gato' === 'gato') && (5 > 2)) {
-  console.log('Verdadeiro');
-} else {
-  console.log('Falso');
+  return `Faltam visitar ${ totalPaises - paisesVisitados} países`;
 }
-//Resposta: Falso
+console.log(totalPaises);
 
-// O que irá aparecer no console?
-if(('Gato' === 'gato') || (5 > 2)) {
-  console.log('Gato' && 'Cão');
-} else {
-  console.log('Falso');
+var profissao = 'Designer';
+
+function dados() {
+  var nome = 'André';
+  var idade = 28;
+  function outrosDados() {
+    var endereco = 'Rio de Janeiro';
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
 }
-//Resposta: Cão.
+
+console.log(dados()); // Retorna 'André, 29, Rio de Janeiro, Designer'
+//outrosDados(); // retorna um erro
