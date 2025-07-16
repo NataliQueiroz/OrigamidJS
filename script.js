8177da8f598a44369dc18aee83f78c11da5c7d04
Natali Queiróz <queiroznatt@gmail.com>
2025-07-16 18:50:27 -0400

    Objetos 1 e 2

--- script.js
+++ script.js
@@ -1,54 +1,31 @@
-// Crie uma função para verificar se um valor é Truthy
-function isTruthy(algo) {
-  return !!algo;
-}
+var pessoa = {
+  nome: "Ian",
+  idade: 28,
+};
+console.log(pessoa);
 
-// Crie uma função matemática que retorne o perímetro de um quadrado
-// lembrando: perímetro é a soma dos quatro lados do quadrado
-function perimetroQuadrado(lado) {
-  return lado * 4;
-}
+var quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+  cinco() {
+    return 5;
+  },
+};
 
-// Crie uma função que retorne o seu nome completo
-// ela deve possuir os parâmetros: nome e sobrenome
-function nomeCompleto(nome, sobrenome) {
-  return `${nome} ${sobrenome}`;
-}
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
 
-// Crie uma função que verifica se um número é par
-function parOuImpar(numero) {
-  if (numero === 0) {
-    return "Seu número é zero";
-  } else if (numero % 2 === 0) {
-    return "Seu número é par";
-  } else {
-    return "Seu número é ímpar";
-  }
-}
-
-// Crie uma função que retorne o tipo de
-// dado do argumento passado nela (typeof)
-function tipoDeDado(param) {
-  return typeof param;
-}
-
-// addEventListener é uma função nativa do JavaScript
-// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
-// utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
-
-addEventListener("scroll", function () {
-  console.log("Laranja Queiróz");
-});
-
-// Corrija o erro abaixo
-var totalPaises = 193;
-function precisoVisitar(paisesVisitados) {
-  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-}
-
-function jaVisitei(paisesVisitados) {
-  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-}
-console.log(precisoVisitar(2));
-console.log(jaVisitei(5));
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: "#84e",
+  metadeHeight() {
+    return this.height / 2;
+  },
+};
+var bg = menu.backgroundColor;
