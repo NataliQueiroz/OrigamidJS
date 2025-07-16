eb8cf3703cc1d1e7a18682a9728682e55d25c43c
Natali Queiróz <queiroznatt@gmail.com>
2025-07-15 23:11:35 -0400

    Exercício de Funções

--- script.js
+++ script.js
@@ -1,75 +1,54 @@
-function areaQuadrado(lado){
-  return lado * lado;
+// Crie uma função para verificar se um valor é Truthy
+function isTruthy(algo) {
+  return !!algo;
 }
 
-console.log(areaQuadrado(2));
-
-function pi() {
-  return 3.14;
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetroQuadrado(lado) {
+  return lado * 4;
 }
 
-var total = 5 * pi(); // 15.7
-console.log(pi());
-
-function imc(peso,altura) {
-    var imc = peso / altura * altura;
-    return imc;
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
 }
-console.log(imc(61, 1,56));
 
-function corFavorita(cor) {
-  if(cor === 'azul') {
-    return 'Você gosta do céu';
-  } else if(cor === 'verde') {
-    return 'Você gosta de mato';
+// Crie uma função que verifica se um número é par
+function parOuImpar(numero) {
+  if (numero === 0) {
+    return "Seu número é zero";
+  } else if (numero % 2 === 0) {
+    return "Seu número é par";
   } else {
-    return 'Você não gosta de nada';
+    return "Seu número é ímpar";
   }
-};
-corFavorita(); 
-
-addEventListener('click', function(){console.log('Oi')});
-
-function imc2(peso, altura) {
-  const imc = peso / (altura ** 2);
-  console.log(imc);
 }
 
-imc2(80, 1.80); // retorna o imc
-console.log(imc2(80, 1.80)); // retorna o imc e undefined
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function tipoDeDado(param) {
+  return typeof param;
+}
 
-function terceiraIdade(idade) {
-  if(typeof idade !== 'number') {
-    return 'Por gentileza preencha um número'
-  }
- else if (idade >= 60){
-    return true;
-  } else {
-    return false;
-  }
-};
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
 
-console.log(terceiraIdade('vaca'));
-  
+addEventListener("scroll", function () {
+  console.log("Laranja Queiróz");
+});
+
+// Corrija o erro abaixo
 var totalPaises = 193;
-function faltaVisitar(paisesVisitados){
-
-  return `Faltam visitar ${ totalPaises - paisesVisitados} países`;
-}
-console.log(totalPaises);
-
-var profissao = 'Designer';
-
-function dados() {
-  var nome = 'André';
-  var idade = 28;
-  function outrosDados() {
-    var endereco = 'Rio de Janeiro';
-    var idade = 29;
-    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
-  }
-  return outrosDados();
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
 }
 
-console.log(dados()); // Retorna 'André, 29, Rio de Janeiro, Designer'
-//outrosDados(); // retorna um erro
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+console.log(precisoVisitar(2));
+console.log(jaVisitei(5));
