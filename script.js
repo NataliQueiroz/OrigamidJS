ae9f0c75e1a47987173ed95f9aea0f94a5b17ec7
Natali Queiróz <queiroznatt@gmail.com>
2025-07-16 20:21:35 -0400

    Tudo é objeto

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-// Crie um objeto com os seus dados pessoais
-// Deve possui pelo menos duas propriedades nome e sobrenome
-var dadosPessoais = {
-  nome: "Natali",
-  sobrenome: "Queiróz",
-};
-dadosPessoais.nomeCompleto = function () {
-  return `${this.nome} ${this.sobrenome}`;
-};
-// Crie um método no objeto anterior, que mostre o seu nome completo
+var nome = "Lua";
 
-// Modifique o valor da propriedade preco para 3000
-var carro = {
-  preco: 1000,
-  portas: 4,
-  marca: "Audi",
-};
-carro.preco = 3000;
+var nomeMinusculo = nome.toLowerCase();
 
-// Crie um objeto de um cachorro que represente um labrador,
-// preto com 10 anos, que late ao ver um homem
-var cachorro = {
-  raça: "labrador",
-  cor: "preto",
-  idade: 10,
-  latir(pessoa) {
-    if (pessoa === "homem") {
-      return "Latir";
-    } else {
-      return "Nada";
-    }
-  },
-};
+var btn = document.querySelector(".btn");
