97ad162977b0e4f69e18e62550ad0a7cb3b6c846
Natali Queiróz <queiroznatt@gmail.com>
2025-07-16 20:34:54 -0400

    Exercícios sobre objetos e métodos

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var nome = "Lua";
+// nomeie 3 propriedades ou métodos de strings
+var comida = "Pipoquinha";
+//length;
+// fixed;
+// match;
 
-var nomeMinusculo = nome.toLowerCase();
-
+// nomeie 5 propriedades ou métodos de elementos do DOM
 var btn = document.querySelector(".btn");
+// addEventListener
+// appendChild
+//id
+//innerText
+//outerHtml
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
