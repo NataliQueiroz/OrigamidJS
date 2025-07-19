b68dc61fb522c6dc40da5f06aea3862d0ebda844
Natali Queiróz <queiroznatt@gmail.com>
2025-07-18 20:49:42 -0400

    Exercício de arrays e loopa

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-//var ultimoItem = videoGames.pop();
-
-// for (var numero = 0; numero <= 4; numero++) {
-//   console.log(numero);
-// }
-// var i = 0;
-// while (i < 10) {
-//   console.log(i);
-//   i++;
-// }
-
-var videoGames = ["Switch", "PS4", "Xbox", "3DS"];
-
-for (var item = 0; item < videoGames.length; item++) {
-  console.log(videoGames[item]);
-  if (videoGames[item] === "PS4") {
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilCampeao = ["1959", "1962", "1970", "1994", "2002"];
+console.log(brasilCampeao);
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (var i = 0; i < brasilCampeao.length; i++) {
+  var ano = `O brasil ganhou a copa de ${brasilCampeao[i]}`;
+  console.log(ano);
+}
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+  if (frutas[i] === "Pera") {
     break;
   }
 }
-var frutas = ["banana", "pera", "maçã", "laranja", "côco"];
-frutas.forEach(function (frutas, index, array) {
-  console.log(frutas, index, array);
-});
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+var fruta = frutas[frutas.length - 1];
+console.log(fruta);
