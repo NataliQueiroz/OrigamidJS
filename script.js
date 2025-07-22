592cc012981accb298d8862326d9069378424059
Natali Queiróz <queiroznatt@gmail.com>
2025-07-22 19:58:44 -0400

    Escopo, let e const

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-// Some 500 ao valor de scroll abaixo,
-// atribuindo o novo valor a scroll
-var scroll = 1000;
-var mais = 500;
-console.log((scroll += mais));
+var carro = "Fusca";
 
-// Atribua true para a variável darCredito,
-// caso o cliente possua carro e casa.
-// E false caso o contrário.
-var possuiCarro = false;
-var possuiCasa = true;
-var darCredito =
-  possuiCarro && possuiCasa ? "Liberar Crédito" : "Crédito Negado";
-console.log(darCredito);
+function mostrarCarro() {
+  var frase = `Meu carro é um ${carro}`;
+  console.log(frase);
+}
+
+mostrarCarro();
+console.log(carro);
+
+if (true) {
+  const mês = "Julho";
+  console.log(mês);
+}
+
+//console.log(mês);
+{
+  var carro2 = "Brasília";
+  const ano = 2025;
+}
+console.log(carro2);
+//console.log(ano);
+var i = 500;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(`Número ${i}`);
+// }
+console.log(i);
+
+const semana = "Sexta";
+
+console.log(semana);
+
+const data = {
+  ano: 2018,
+  mes: "dezembro",
+};
+console.log((data.ano = 2019));
