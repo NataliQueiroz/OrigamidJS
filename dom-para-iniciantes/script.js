03f024a5c5a2b72ae77eb508a1a13ee3e54e5c60
Natali Queiróz <queiroznatt@gmail.com>
2025-07-25 20:15:12 -0400

    Entendendo o DOM 1 e 2

--- dom-para-iniciantes/script.js
+++ dom-para-iniciantes/script.js
@@ -1,1 +1,6 @@
-window.alert("Isso Mesmo");
+//window.alert("Isso Mesmo");
+const h1Selecionado = document.querySelector("h1");
+
+h1Selecionado.addEventListener("click", function () {
+  console.log("Clicou em ", h1Selecionado.innerText);
+});
