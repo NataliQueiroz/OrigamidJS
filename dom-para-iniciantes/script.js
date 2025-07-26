e95036351717697a8aa881b713cd2a489fa34b64
Natali Queiróz <queiroznatt@gmail.com>
2025-07-25 20:58:00 -0400

    Exercício de DOM

--- dom-para-iniciantes/script.js
+++ dom-para-iniciantes/script.js
@@ -1,6 +1,13 @@
-//window.alert("Isso Mesmo");
-const h1Selecionado = document.querySelector("h1");
+// Retorne o url da página atual utilizando o objeto window
+const href = window.location.href;
+console.log(href);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+// Retorne a linguagem do navegador
+const linguagemNavegador = navigator.language;
+console.log(linguagemNavegador);
 
-h1Selecionado.addEventListener("click", function () {
-  console.log("Clicou em ", h1Selecionado.innerText);
-});
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
