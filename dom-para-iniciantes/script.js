05f1fa66e9dc011f6e0d6d57a57857ddb5f7038e
Natali Queiróz <queiroznatt@gmail.com>
2025-07-31 19:13:21 -0400

    Exercício de seleção de elementos

--- dom-para-iniciantes/script.js
+++ dom-para-iniciantes/script.js
@@ -1,13 +1,15 @@
-// Retorne o url da página atual utilizando o objeto window
-const href = window.location.href;
-console.log(href);
-// Seleciona o primeiro elemento da página que
-// possua a classe ativo
-const elementoAtivo = document.querySelector(".ativo");
-// Retorne a linguagem do navegador
-const linguagemNavegador = navigator.language;
-console.log(linguagemNavegador);
-
-// Retorne a largura da janela
-const larguraJanela = window.innerWidth;
-console.log(larguraJanela);
+// Retorne no console todas as imagens do site
+const imagensSite = document.querySelectorAll("img");
+console.log(imagensSite);
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+// // const imagensAnimais = document.querySelectorAll("img[src ^= img/imagem]");
+// console.log(imagensAnimais);
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll("[href^='#']");
+console.log(linksInternos);
+// Selecione o primeiro h2 dentro de .animais-descricao
+const selecionarH2 = document.querySelector("h2");
+console.log(selecionarH2);
+// Selecione o último p do site
+const ultimoP = document.querySelector(".animais-descrição p:last-child");
+console.log(ultimoP);
